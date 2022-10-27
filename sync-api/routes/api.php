e00466fdce68992bf5d9e92dbdd348a1770fe336
bjausseran <benoit.jausseran@ynov.com>
2022-10-27 10:59:01 +0200

    laravel : db management

--- sync-api/routes/api.php
+++ sync-api/routes/api.php
@@ -20,3 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('/captor', 'CaptorController');
 Route::apiResource('/client', 'ClientController');
+
+
+
+// here for test, should be deleted before export
+Route::get('/test', function(){
+    return DB::connection(env("DBLOCAL_NAME"))->table("Captors")->get();
+});
+
